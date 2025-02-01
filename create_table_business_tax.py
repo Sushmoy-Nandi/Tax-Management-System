19896bf65d6349b9ad9bf4525793b1c9d810c84f
“Sushmoy <learnsushmoysk1091@gmail.com>
2025-02-01 21:08:34 +0600

    creating business tax table

---
+++ create_table_business_tax.py
@@ -0,0 +1,27 @@
+import mysql.connector
+
+db_name = "tax_management_system"
+
+mydbconnection=mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="#sqsks@1091l",
+    database=db_name
+)
+
+mycursor = mydbconnection.cursor()
+
+sqlquery = """
+            CREATE TABLE BusinessTax 
+            (
+                BusinessTaxID INT PRIMARY KEY AUTO_INCREMENT,
+                BusinessName VARCHAR(255),  -- Name of the business
+                BusinessType VARCHAR(100),  -- Type of business (e.g., Retail, Manufacturing)
+                AnnualRevenue DECIMAL(10, 2),  -- Total revenue for the year
+                TaxPayerNID INT
+            );
+
+            """
+
+mycursor.execute(sqlquery)
+print("Create Business Tax  table successful")
