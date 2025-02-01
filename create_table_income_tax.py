c48b2e07cb980c6449df4b654b80aaeaae6ce88d
“Sushmoy <learnsushmoysk1091@gmail.com>
2025-02-01 21:10:36 +0600

    creating income tax table

---
+++ create_table_income_tax.py
@@ -0,0 +1,26 @@
+import mysql.connector
+
+db_name = "tax_management_system"
+
+mydbconnection=mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="#sqsks@1091l",
+    database=db_name
+)
+
+mycursor = mydbconnection.cursor()
+
+sqlquery = """
+            CREATE TABLE IncomeTax 
+            (
+                IncomeTaxID INT PRIMARY KEY AUTO_INCREMENT,
+                AnnualIncome DECIMAL(10, 2),  -- Total annual income
+                TaxableIncome DECIMAL(10, 2), -- Portion of the income that is taxable
+                TaxPayerNID INT
+            );
+
+            """
+
+mycursor.execute(sqlquery)
+print("Create Income tax table successful")
