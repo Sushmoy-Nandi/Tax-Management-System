8a0d59500f5ddd47f3c3de618ce317ac020f68ca
“Sushmoy <learnsushmoysk1091@gmail.com>
2025-02-02 23:31:21 +0600

    creating land tax table

---
+++ create_table_land_tax.py
@@ -0,0 +1,28 @@
+import mysql.connector
+
+db_name = "tax_management_system"
+
+mydbconnection=mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="#sqsks@1091l",
+    database=db_name
+)
+
+mycursor = mydbconnection.cursor()
+
+sqlquery = """
+        CREATE TABLE LandTax (
+    LandTaxID INT PRIMARY KEY AUTO_INCREMENT,
+    PropertyLocation VARCHAR(255), 
+    PropertySize DECIMAL(10, 2), 
+    PropertyValue DECIMAL(10, 2),
+    TaxPayerNID INT
+    
+);
+
+
+            """
+
+mycursor.execute(sqlquery)
+print("Create land tax table successful")
